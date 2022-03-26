5b16b1019e008fd5cd4fddc330be843bed1e7cf6
maxjesionka <80617874+maxjesionka@users.noreply.github.com>
2022-03-26 12:46:24 +0100

    26.03

---
+++ demo/demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/example")
+    public String plik() {
+        return "plik.html"; // plik.html w templates
+    }
+}
